0ed22ab90fe52c4f239b0c3f0195f200450ba8a6
kubatbaew <kubatbaew@gmail.com>
2025-05-28 09:11:56 +0600

    add two url login and home

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from apps.pages.views import login_logics, homepage
+
 
 urlpatterns = [
-    
+    path('', homepage, name="homepage"),
+    path('login/', login_logics, name="login_logics"),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+from functools import wraps
+
+def _check_is_authenticated(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+
+        if not request.user.is_authenticated:
+            return redirect('login_logics')
+        return view_func(request, *args, **kwargs)
+    
+    return _wrapped_view
+
+
+
+
+def login_logics(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        remember_me = request.POST.get('remember_me')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            if not remember_me:
+                request.session.set_expiry(0)
+
+            return redirect('homepage')
+        else:
+            messages.error(request, "Неверное имя пользователя или пароль")
+
+    return render(request, "login.html")
+
+
+@_check_is_authenticated
+def homepage(request):
+
+    return render(request, 'index.html', locals())
+
